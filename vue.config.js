784e0bbb4507044fa0a0a045e2a7be9a7ae6772a
Drew Perry <me@drewperry.co.uk>
2021-12-28 00:33:11 +0000

    Bring in prevous build details

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,29 @@
 module.exports = {
     pluginOptions: {
       electronBuilder: {
-        preload: 'src/preload.js'
+        preload: 'src/preload.js',
+        builderOptions: { 
+          "productName": "DeviceKontrol",
+          "appId": "solutions.alteka.devicekontrol",
+          "directories": {
+            "output": "build"
+          },
+          "files": [
+            "dist/electron/**/*"
+          ],
+          "mac": {
+            "icon": "build/icons/icon.png",
+            "target": "pkg"
+          },
+          "win": {
+            "icon": "build/icons/icon.png"
+          },
+          "nsis": {
+            "oneClick": false,
+            "createDesktopShortcut": false,
+            "menuCategory": true
+          }
+        }
       }
     }
   }
